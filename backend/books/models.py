8b81e6d1bb4d4a669094242d92dacb3d9dfb92f0
Taniya Singh <taniya.singh12@gmail.com>
2021-02-24 10:51:42 -0500

    Revert "updating .gitignore"

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,16 +1,3 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
 
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    content = models.TextField()
-    ISBN = models.CharField(max_length=13)
-    DDC = models.DecimalField(max_digits=6, decimal_places=2)
-    date_added = models.DateTimeField(default=timezone.now)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
+# Create your models here.
