aea2cc81434317ef546331e6fa2d557be67c07d9
tas09009 <taniya.singh12@gmail.com>
2021-02-24 10:44:04 -0500

    updating .gitignore

--- backend/bookrec/urls.py
+++ backend/bookrec/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls)
+    # path('', views.home)
     # path('', include('frontend.urls')),
 ]
