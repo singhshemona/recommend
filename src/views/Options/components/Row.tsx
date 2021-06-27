70dde4327367a27cb24d59a4021fb8eef7ba49f6
Shemona Singh <singhshemona@gmail.com>
2021-06-27 15:49:28 -0700

    added more styling to books, adjusted height and textand colors to bookshelf

--- src/views/Options/components/Row.tsx
+++ src/views/Options/components/Row.tsx
@@ -3,15 +3,36 @@ import styled from 'styled-components';
 
 const Shelf = styled.div`
   display: flex;
+  overflow: scroll;
+  background: #f9debb;
+  padding: 10px 20px;
 `
 
 const Book = styled.div`
   border: 1px solid black;
+  max-height: 400px;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  background: white;
+  margin: 0 10px 0 10px;
+  padding: 4px 8px;
 `
 
 const Class = styled.h2`
   text-align: center;
   background: burlywood;
+  margin-top: 0;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 36px;
+  border: 1px solid black;
+`
+
+const Title = styled.p`
+  transform: rotate(180deg);
+  writing-mode: vertical-rl;
 `
 
 type Props = {
@@ -28,7 +49,7 @@ export const Row = ({ heading, books }: Props) => {
       <Shelf>
         {books.map((book) => 
           <Book>
-            <p>{book.title}</p>
+            <Title>{book.title}</Title>
             <p>{book.classify_DDC}</p>
           </Book> 
         )} 
