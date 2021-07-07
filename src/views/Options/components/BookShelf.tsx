74f8d66cfa33c78002913292423ba1d4d9d16512
Shemona Singh <singhshemona@gmail.com>
2021-07-06 21:44:54 -0700

    data retrieval and mapping onto graph works now yaaay. just not sure if it's scalable

--- src/views/Options/components/BookShelf.tsx
+++ src/views/Options/components/BookShelf.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { Row } from './Row';
 import axios from 'axios';
 import styled from 'styled-components';
 import { ResponsiveCirclePacking } from '@nivo/circle-packing'
@@ -10,7 +9,7 @@ const Container = styled.div`
 
 export const BookShelf = () => {
   const [ zoomedId, setZoomedId ] = useState<string | null>(null)
-  const [ loading, setLoading ] = useState(true)
+  const [ loading, setLoading ] = useState(false)
   const [ books, setBooks ] = useState([{
     "title": "ultrices mattis odio donec vitae nisi",
     "classify_DDC": 132.174,
@@ -26,45 +25,87 @@ export const BookShelf = () => {
       .catch((err) => console.log(err));
   }, [])
 
-  let literature:any = [];
-  let historyAndGeography:any = [];
-  let rest:any = [];
-  
-  let emptyTest:any = [];
+  // const literature = {
+  //   "name": "Literature",
+  //   "children": [{
+  //     "name": "Book Title",
+  //     "loc": 1
+  //   }]
+  // }
 
+  // const historyAndGeography = {
+  //   "name": "History and Geography",
+  //   "children": [{
+  //     "name": "Book Title",
+  //     "loc": 1
+  //   }]
+  // }
+
+  // const rest = {
+  //   "name": "Rest",
+  //   "children": [{
+  //     "name": "Book Title",
+  //     "loc": 1
+  //   }]
+  // }
+
+  // const data = {
+  //   ...literature,
+  //   ...historyAndGeography,
+  //   ...rest
+  // }
+
+  const data = {
+    id: 'data',
+    value: 0,
+    children: [
+      {
+        id: 'literature',
+        value: 1,
+        children: [{
+          "name": "Book Title",
+          "loc": 1
+        }]
+      },
+      {
+        id: 'history',
+        value: 1,
+        children: [{
+          "name": "Book Title",
+          "loc": 1
+        }]
+      },
+      {
+        id: 'rest',
+        value: 1,
+        children: [{
+          "name": "Book Title",
+          "loc": 1
+        }]
+      }
+    ]
+  }
+  
   books.forEach(book => 
     {
       if (book.classify_DDC > 800 && book.classify_DDC < 900) {
-        literature.push(book)
+          data.children[0].children.push({
+            "name": book.title,
+            "loc": 1
+          })
       } else if (book.classify_DDC > 900 && book.classify_DDC < 1000) {
-        historyAndGeography.push(book)
+          data.children[1].children.push({
+            "name": book.title,
+            "loc": 1
+          })
       } else {
-        rest.push(book)
+          data.children[2].children.push({
+            "name": book.title,
+            "loc": 1
+          })
       }
     }
   )
-
-  const data = {
-    "name": "Literature",
-    "children": [
-        {
-          "name": "Normal People", // id
-          "loc": 1 // value
-        },
-        {
-          "name": "Call Me By Your Name",
-          "loc": 1
-        },
-        {
-          "name": "A Thousand Splendid Suns",
-          "loc": 1
-        },
-        {
-          "name": "Radical Acceptance",
-          "loc": 1
-        }
-    ]
-  }
 
   return (
     loading ? 
@@ -97,10 +138,6 @@ export const BookShelf = () => {
         colors={{ scheme: 'nivo' }}
         childColor={{ from: 'color', modifiers: [ [ 'brighter', 0.4 ] ] }}
         padding={4}
-        enableLabels={true}
-        labelsFilter={function(e){return 2===e.node.depth}}
-        labelsSkipRadius={10}
-        labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
         borderWidth={1}
         borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.5 ] ] }}
         defs={[
